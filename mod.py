ca2767eb62d7456e8be15a3782ecf7381315a87d
AnakLK <682020069@student.uksw.edu>
2023-08-29 23:34:10 +0700

    add Sesi 3 and README

---
+++ mod.py
@@ -0,0 +1,15 @@
+s = "Hacktiv8-PTP Python for Data Science"
+a = [100, 200, 300]
+
+def foo(arg):
+    print('arg = {arg}')
+
+class Foo:
+    pass
+
+if (__name__== '__main__'): 
+    print(s)
+    print(a)
+    foo('quux')
+    x = Foo()
+    print(x)
